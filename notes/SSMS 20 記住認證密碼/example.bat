5927b3ceae5f4d20d96357cc44020858349b77bf
acc2git <acc2gml@gmail.com>
2024-11-12 09:41:08 +0800

    add example

---
+++ notes/SSMS 20 記住認證密碼/example.bat
@@ -0,0 +1,3 @@
+cmdkey /generic:Microsoft:SSMS:20:192.168.100.100:user001:8c91a03d-f9b4-46c0-a305-b5dcc79ff907:1 /user:user001 /pass:pwd001
+cmdkey /generic:Microsoft:SSMS:20:192.168.100.101:user001:8c91a03d-f9b4-46c0-a305-b5dcc79ff907:1 /user:user001 /pass:pwd001
+pause
