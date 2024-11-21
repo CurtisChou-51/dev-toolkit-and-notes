08532656cf6a0995b449e07cff5c7f360ba7809b
acc2git <acc2gml@gmail.com>
2024-11-21 16:10:48 +0800

    SQL Server DDL 語法應用筆記

---
+++ notes/SQL Server DDL 語法應用筆記/example.sql
@@ -0,0 +1,25 @@
+
+/* 檢查資料表是否存在，不存在則建立 */
+if not exists (select 1
+                 from sysobjects
+                where id = object_id('XXXTable')
+                  and type = 'U')
+begin
+    create table XXXTable (
+       Id1         bigint        not null,
+       XXXColumn   Nvarchar(30)  null,
+       constraint PK_XXXTable primary key (Id1)
+    );
+end;
+
+/* 檢查欄位是否存在，不存在則新增 */
+if not exists (select 1 
+                 from sys.columns c 
+                where c.object_id = object_id('XXXTable') 
+                  and c.name = 'XXXColumn')
+begin
+    alter table XXXTable add XXXColumn Nvarchar(30) not null default '0';
+end;
+
+/* 修改欄位 */
+alter table XXXTable alter column XXXColumn Nvarchar(30) null;
