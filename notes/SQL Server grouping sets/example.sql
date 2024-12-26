1e145f0981b3b41f6ae3c90e5d35a2ea715dbae0
CurtisChou-51 <71554373+CurtisChou-51@users.noreply.github.com>
2024-12-26 10:30:57 +0800

    update example sql

--- notes/SQL Server grouping sets/example.sql
+++ notes/SQL Server grouping sets/example.sql
@@ -1,31 +1,24 @@
-drop table if exists #Sales;
-CREATE TABLE #Sales (
-    Subsidiaries varchar(15),
-    Department varchar(15),
-    ProductId varchar(15),
-    Quantity decimal(10,0)
-);
-
--- 測試資料
-insert into #Sales (Subsidiaries, Department, ProductId, Quantity) values
-    ('SubA', 'DeptA', '1', 1000),
-    ('SubA', 'DeptB', '2', 1500),
-    ('SubA', 'DeptC', '3', 800),
-    ('SubB', 'DeptA', '1', 500),
-    ('SubB', 'DeptB', '5', 600),
-    ('SubB', 'DeptC', '6', 700),
-    ('SubA', 'DeptA', '1', 1200),
-    ('SubA', 'DeptA', '2', 900),
-    ('SubB', 'DeptB', '5', 800),
-    ('SubA', 'DeptC', '6', 800),
-    ('SubB', 'DeptC', '6', 500),
-    ('SubA', 'DeptB', '5', 700),
-    ('SubA', 'DeptB', '2', 1000),
-    ('SubB', 'DeptA', '6', 1600),
-    ('SubB', 'DeptB', '2', 600),
-    ('SubB', 'DeptB', '3', 800),
-    ('SubB', 'DeptC', '5', 1200)
-;
+select Subsidiaries, Department, ProductId, Quantity
+  into #Sales
+  from ( values 
+      ('SubA', 'DeptA', '1', 1000),
+      ('SubA', 'DeptB', '2', 1500),
+      ('SubA', 'DeptC', '3', 800),
+      ('SubB', 'DeptA', '1', 500),
+      ('SubB', 'DeptB', '5', 600),
+      ('SubB', 'DeptC', '6', 700),
+      ('SubA', 'DeptA', '1', 1200),
+      ('SubA', 'DeptA', '2', 900),
+      ('SubB', 'DeptB', '5', 800),
+      ('SubA', 'DeptC', '6', 800),
+      ('SubB', 'DeptC', '6', 500),
+      ('SubA', 'DeptB', '5', 700),
+      ('SubA', 'DeptB', '2', 1000),
+      ('SubB', 'DeptA', '6', 1600),
+      ('SubB', 'DeptB', '2', 600),
+      ('SubB', 'DeptB', '3', 800),
+      ('SubB', 'DeptC', '5', 1200)
+  ) as temp_table(Subsidiaries, Department, ProductId, Quantity);
 
 select
     case grouping_id(Subsidiaries, Department, ProductId)
