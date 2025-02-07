e121755c8bf9b7393dcb7847fdc59034ac03f9a5
CurtisChou-51 <71554373+CurtisChou-51@users.noreply.github.com>
2025-02-07 09:23:53 +0800

    更新敘述、tab -> space

--- notes/C# Refact Note - FilterData/FilterData_after.cs
+++ notes/C# Refact Note - FilterData/FilterData_after.cs
@@ -1,37 +1,37 @@
 private List<EventCsvDto> FilterData(List<BankRawDataDto> rawDatas, string fileName, DateTime filterDate)
 {
-	var existData = GetBankData(filterDate)
-		.Select(x => new { x.CaseNumber, x.BankCode, x.BankAccount, x.BankIdno })
-		.ToHashSet();
+    var existData = GetBankData(filterDate)
+        .Select(x => new { x.CaseNumber, x.BankCode, x.BankAccount, x.BankIdno })
+        .ToHashSet();
 
-	return rawDatas.Where(IsValidData)
-		.GroupBy(x => new { x.CaseNumber, x.BankCode, x.BankAccount, x.BankIdno })
-		.Where(g => !existData.Contains(g.Key))
-		.Select(g => g.First())
-		.Select(x => ConvertToEventCsvDto(fileName, x))
-		.ToList();
+    return rawDatas.Where(IsValidData)
+        .GroupBy(x => new { x.CaseNumber, x.BankCode, x.BankAccount, x.BankIdno })
+        .Where(g => !existData.Contains(g.Key))
+        .Select(g => g.First())
+        .Select(x => ConvertToEventCsvDto(fileName, x))
+        .ToList();
 }
 
 private static EventCsvDto ConvertToEventCsvDto(string fileName, BankRawDataDto rawData)
 {
-	return new EventCsvDto
-	{
-		SrcFileName = fileName,
-		CaseNumber = rawData.CaseNumber,
-		WarningDatetime = ToDateTime(rawData.WarningDate),
-		EventType = ConvertEventType(rawData.FraudMethod),
-		BankCode = rawData.BankCode,
-		BankAccount = rawData.BankAccount,
-		BankIdno = rawData.BankIdno,
-		BankUserName = rawData.BankUserName?.Replace("　", "")?.Trim() ?? ""
-	};
+    return new EventCsvDto
+    {
+        SrcFileName = fileName,
+        CaseNumber = rawData.CaseNumber,
+        WarningDatetime = ToDateTime(rawData.WarningDate),
+        EventType = ConvertEventType(rawData.FraudMethod),
+        BankCode = rawData.BankCode,
+        BankAccount = rawData.BankAccount,
+        BankIdno = rawData.BankIdno,
+        BankUserName = rawData.BankUserName?.Replace("　", "")?.Trim() ?? ""
+    };
 }
 
 private static bool IsValidData(BankRawDataDto rawData)
 {
-	return !string.IsNullOrEmpty(rawData.CaseNumber)
-		&& !string.IsNullOrEmpty(rawData.BankCode)
-		&& !string.IsNullOrEmpty(rawData.BankAccount)
-		&& !string.IsNullOrEmpty(rawData.BankIdno)
-		&& rawData.FraudMethod is ("fakeinvestment" or "fakefriends");
+    return !string.IsNullOrEmpty(rawData.CaseNumber)
+        && !string.IsNullOrEmpty(rawData.BankCode)
+        && !string.IsNullOrEmpty(rawData.BankAccount)
+        && !string.IsNullOrEmpty(rawData.BankIdno)
+        && rawData.FraudMethod is ("fakeinvestment" or "fakefriends");
 }
--- notes/C# Refact Note - FilterData/FilterData_before.cs
+++ notes/C# Refact Note - FilterData/FilterData_before.cs
@@ -1,49 +1,49 @@
 private List<EventCsvDto> FilterData(List<BankRawDataDto> rawDatas, string fileName, DateTime filterDate)
 {
-	var existData = GetBankData(filterDate);
+    var existData = GetBankData(filterDate);
 
-	List<string> listindex = new List<string>();
-	List<EventCsvDto> listCsv = new List<EventCsvDto>();
+    List<string> listindex = new List<string>();
+    List<EventCsvDto> listCsv = new List<EventCsvDto>();
 
-	foreach (BankRawDataDto rawData in rawDatas)
-	{
-		if (string.IsNullOrEmpty(rawData.CaseNumber))
-			continue;
-		if (string.IsNullOrEmpty(rawData.BankCode))
-			continue;
-		if (string.IsNullOrEmpty(rawData.BankAccount))
-			continue;		
-		if (string.IsNullOrEmpty(rawData.BankIdno))
-			continue;
-		if (rawData.FraudMethod != "fakeinvestment" && rawData.FraudMethod != "fakefriends")
-			continue;
+    foreach (BankRawDataDto rawData in rawDatas)
+    {
+        if (string.IsNullOrEmpty(rawData.CaseNumber))
+            continue;
+        if (string.IsNullOrEmpty(rawData.BankCode))
+            continue;
+        if (string.IsNullOrEmpty(rawData.BankAccount))
+            continue;        
+        if (string.IsNullOrEmpty(rawData.BankIdno))
+            continue;
+        if (rawData.FraudMethod != "fakeinvestment" && rawData.FraudMethod != "fakefriends")
+            continue;
 
-		EventCsvDto EventCsvDto = new()
-		{
-			SrcFileName = fileName,
-			CaseNumber = rawData.CaseNumber,
-			WarningDatetime = ToDateTime(rawData.WarningDate),
-			EventType = ConvertEventType(rawData.FraudMethod),
-			BankCode = rawData.BankCode,
-			BankAccount = rawData.BankAccount,
-			BankIdno = rawData.BankIdno,
-			BankUserName = rawData.BankUserName?.Replace("　", "")?.Trim() ?? ""
-		};
+        EventCsvDto EventCsvDto = new()
+        {
+            SrcFileName = fileName,
+            CaseNumber = rawData.CaseNumber,
+            WarningDatetime = ToDateTime(rawData.WarningDate),
+            EventType = ConvertEventType(rawData.FraudMethod),
+            BankCode = rawData.BankCode,
+            BankAccount = rawData.BankAccount,
+            BankIdno = rawData.BankIdno,
+            BankUserName = rawData.BankUserName?.Replace("　", "")?.Trim() ?? ""
+        };
 
-		string strindex = EventCsvDto.CaseNumber + EventCsvDto.BankCode + EventCsvDto.BankAccount + EventCsvDto.BankIdno;
-		if (listindex.Contains(strindex))
-			continue;
+        string strindex = EventCsvDto.CaseNumber + EventCsvDto.BankCode + EventCsvDto.BankAccount + EventCsvDto.BankIdno;
+        if (listindex.Contains(strindex))
+            continue;
 
-		int cnt = existData.Where(p => p.CaseNumber == EventCsvDto.CaseNumber
-								&& p.BankCode == EventCsvDto.BankCode
-								&& p.BankAccount == EventCsvDto.BankAccount
-								&& p.BankIdno == EventCsvDto.BankIdno).ToList().Count;
-		if (cnt > 0)
-			continue;
+        int cnt = existData.Where(p => p.CaseNumber == EventCsvDto.CaseNumber
+                                && p.BankCode == EventCsvDto.BankCode
+                                && p.BankAccount == EventCsvDto.BankAccount
+                                && p.BankIdno == EventCsvDto.BankIdno).ToList().Count;
+        if (cnt > 0)
+            continue;
 
-		listindex.Add(strindex);
-		listCsv.Add(EventCsvDto);
-	}
+        listindex.Add(strindex);
+        listCsv.Add(EventCsvDto);
+    }
 
-	return listCsv;
+    return listCsv;
 }
