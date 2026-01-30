943d416c092e8efcf9383f5731a87502f29d756a
CurtisChou-51 <acc2gml@gmail.com>
2026-01-30 15:52:51 +0800

    feat(notes): System Outbox Pattern 設計 add example "BlazorAppOutbox"

---
+++ notes/System Outbox Pattern 設計/BlazorAppOutbox/BlazorAppOutbox/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppOutbox
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Outbox> Outboxes { get; set; }
+    }
+
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+
+    public class Outbox
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? EventType { get; set; }
+        public string? Payload { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Status { get; set; }
+        public DateTime? ProcessedAt { get; set; }
+    }
+}
